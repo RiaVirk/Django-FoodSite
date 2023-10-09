797f892746e2d80e0c880b51041df08071f6997d
Maria Virk <121379426+RiaVirk@users.noreply.github.com>
2023-10-09 17:15:43 +0200

    Foundation set

--- foodzone/urls.py
+++ foodzone/urls.py
@@ -4,5 +4,9 @@ from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index,name="index" ),
+    path('', views.index, name="index"),
+    path('contact/', views.contact, name="contact"),
+    path('about/', views.about, name="about"),
+    path('team/', views.team, name="team"),
+    path('menu/', views.all_dishes, name="all_dishes"),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, 'index.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def team(request):
+    return render(request, 'team.html')
+
+
+def all_dishes(request):
+    return render(request, 'contact.html')
