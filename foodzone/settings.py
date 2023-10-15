9ba21d0671c1541bd3570ceac5c286fd8da60ce3
Maria Virk <121379426+RiaVirk@users.noreply.github.com>
2023-10-15 23:18:04 +0200

    HTML Files updated

--- foodzone/settings.py
+++ foodzone/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myapp',
+    'paypal.standard.ipn',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,11 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# MEDIA_URL = "/media/"
+# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+PAYPAL_RECEIVER_EMAIL = 'sts.amandeepkaur@gmail.com'
+PAYPAL_TEST = True
+
+HOST = '127.0.0.1:8000'
--- myapp/views.py
+++ myapp/views.py
@@ -1,13 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse
 from myapp.models import Contact, Dish, Team, Category, Profile, Order
-from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate, logout
-from django.shortcuts import get_object_or_404
+from paypal.standard.forms import PayPalPaymentsForm
+from django.conf import settings
 
 
 def index(request):
-    return render(request, 'index.html')
+    context = {}
+    cats = Category.objects.all().order_by('name')
+    context['categories'] = cats
+    # print()
+    dishes = []
+    for cat in cats:
+        dishes.append({
+            'cat_id': cat.id,
+            'cat_name': cat.name,
+            'cat_img': cat.image,
+            'items': list(cat.dish_set.all().values())
+        })
+    context['menu'] = dishes
+    return render(request, 'index.html', context)
 
 
 def contact_us(request):
@@ -18,14 +32,9 @@ def contact_us(request):
         sub = request.POST.get("subject")
         msz = request.POST.get("message")
 
-        newMessage = Contact(
-            name=name,
-            email=em,
-            subject=sub,
-            message=msz,
-        )
-        newMessage.save()
-        context['message'] = f"Dear {name}, Thanks for your message!"
+        obj = Contact(name=name, email=em, subject=sub, message=msz)
+        obj.save()
+        context['message'] = f"Dear {name}, Thanks for your time!"
 
     return render(request, 'contact.html', context)
 
@@ -96,8 +105,8 @@ def signin(request):
         if check_user:
             login(request, check_user)
             if check_user.is_superuser or check_user.is_staff:
-                return HttpResponseRedirect('/admin/')
-            return HttpResponseRedirect('/dashboard/')
+                return HttpResponseRedirect('/admin')
+            return HttpResponseRedirect('/dashboard')
         else:
             context.update(
                 {'message': 'Invalid Login Details!', 'class': 'alert-danger'})
@@ -148,9 +157,59 @@ def dashboard(request):
     orders = Order.objects.filter(
         customer__user__id=request.user.id).order_by('-id')
     context['orders'] = orders
-    return render(request, 'dashboard.html/', context)
+    return render(request, 'dashboard.html', context)
 
 
 def user_logout(request):
     logout(request)
     return HttpResponseRedirect('/')
+
+
+def single_dish(request, id):
+    context = {}
+    dish = get_object_or_404(Dish, id=id)
+
+    if request.user.is_authenticated:
+        cust = get_object_or_404(Profile, user__id=request.user.id)
+        order = Order(customer=cust, item=dish)
+        order.save()
+        inv = f'INV0000-{order.id}'
+
+        # paypal_dict = {
+        #     'business': settings.PAYPAL_RECEIVER_EMAIL,
+        #     'amount': dish.discounted_price,
+        #     'item_name': dish.name,
+        #     'user_id': request.user.id,
+        #     'invoice': inv,
+        #     'notify_url': 'http://{}{}'.format(settings.HOST, reverse('paypal-ipn')),
+        #     'return_url': 'http://{}{}'.format(settings.HOST, reverse('payment_done')),
+        #     'cancel_url': 'http://{}{}'.format(settings.HOST, reverse('payment_cancel')),
+        # }
+
+        order.invoice_id = inv
+        order.save()
+        request.session['order_id'] = order.id
+
+        # form = PayPalPaymentsForm(initial=paypal_dict)
+        # context.update({'dish': dish, 'form': form})
+
+    return render(request, 'dish.html', context)
+
+
+def payment_done(request):
+    pid = request.GET.get('PayerID')
+    order_id = request.session.get('order_id')
+    order_obj = Order.objects.get(id=order_id)
+    order_obj.status = True
+    order_obj.payer_id = pid
+    order_obj.save()
+
+    return render(request, 'payment_successfull.html')
+
+
+def payment_cancel(request):
+    # remove comment to delete cancelled order
+    # order_id = request.session.get('order_id')
+    # Order.objects.get(id=order_id).delete()
+
+    return render(request, 'payment_failed.html')
